d16dfed897a3d6e7fb8a9ba636d6ee178c7e1f31
Keivy Mora <mkeivy.mora@gmail.com>
2022-10-17 15:19:38 -0400

    modifique authProvider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
       console.info(error.code, error.message);
       navigate("/");
     }
-  }, [userAuth]);
+  }, [userAuth, navigate]);
 
   useEffect(() => {
     refreshToken();
