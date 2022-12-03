30c0b6a45258cdceea88d3e8b6907b16bb6eb3d3
Keivy Mora <mkeivy.mora@gmail.com>
2022-12-02 21:54:28 -0400

    updated axiosconfig

--- src/utils/AxiosConfig.js
+++ src/utils/AxiosConfig.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API;
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-
-axios.defaults.headers.patch["Access-Control-Allow-Origin"] =
-  "https://quiniela-cis.netlify.app";
-
-axios.defaults.headers.put["Access-Control-Allow-Origin"] =
-  "https://quiniela-cis.netlify.app";
 
 export const AxiosConfig = axios;
